a0ce421759ef03b088a193986adf820adc1a8938
fulldump <gerardooscarjt@gmail.com>
2023-02-15 06:17:57 +0100

    feat: add example

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"hello": "world",
+			})
+
+		}),
+	}
+
+	s.ListenAndServe()
+
+}
