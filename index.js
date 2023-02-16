d49e1db344d98761d74b4059ca36346f615984a8
Ivan Gallego <ivan.gallego@iqmetrix.com>
2023-02-16 16:13:11 +0000

    Adding /resumen command and responses in threads

--- index.js
+++ index.js
@@ -15,27 +15,40 @@ const conversations = new Map();
 const membersPerChannel = new Map();
 let api;
 
-app.event('app_mention', ({event, say}) => {
-    const text = event.text.replace(`<@${botId}>`, '').trim();
+app.event('app_mention', ({event, say, payload}) => {
+    if (event.user !== botId) {
+        const text = event.text.replace(`<@${botId}>`, '').trim();
 
-    if (text.length) {
-        chatGPT(text, event.channel).then(answer => {
-            say(answer)
-        });
-    } else {
-        say(`Que coño quieres <@${event.user}>?!`);
+        if (text.length) {
+            chatGPT(text, event.channel).then(answer => {
+                say({text: answer.text, thread_ts: event.thread_ts || event.ts, reply_broadcast: true});
+            });
+        } else {
+            say(`Que coño quieres <@${event.user}>?!`);
+        }
+
+        console.log('p', payload);
     }
+});
 
-    // console.log('p', payload);
+app.command('/resumen', async ({command, ack, respond, payload}) => {
+    await ack();
+
+    const text = await formatConversationHistory(command.channel_id);
+    const answer = await chatGPT(text, command.channel_id, false);
+    console.log('p', payload);
+
+    await respond(answer);
 });
 
 // Start your app
 (async () => {
     await app.start(process.env.PORT || 3000);
+    await app.client.chat.postMessage({channel: 'C04PRHW329Y', text: 'Bot started!'})
     console.log('Bot is running!');
 })();
 
-async function chatGPT(msg, channel) {
+async function chatGPT(msg, channel, useContext = true) {
     let members = membersPerChannel.get(channel);
     if (!members) {
         members = (await app.client.conversations.members({channel})).members.map(m => `<@${m}>`).join(', ');
@@ -50,7 +63,7 @@ async function chatGPT(msg, channel) {
         });
     }
 
-    let conversationParams = conversations.get(channel);
+    let conversationParams = useContext ? conversations.get(channel) : null;
     if (conversationParams) {
         const diff = (new Date()).getTime() - conversationParams.timestamp;
         if (diff > 10 * 60 * 1000) {
@@ -58,14 +71,33 @@ async function chatGPT(msg, channel) {
         }
     }
 
+    let promptPrefix = 'Eres un chat bot, intenta responder las preguntas de la manera mas detallada posible\n\n';
+    if (useContext) {
+        promptPrefix += `Los usuarios de este canal son: ${members}\n\n`;
+    }
+
     const response = await api.sendMessage(msg, {
         ...conversationParams,
-        promptPrefix: `Los usuarios de este canal son: ${members}`
+        promptPrefix
     });
-    conversations.set(channel, {
-        conversationId: response.conversationId,
-        parentMessageId: response.id,
-        timestamp: (new Date()).getTime()
-    })
+
+    if (useContext) {
+        conversations.set(channel, {
+            conversationId: response.conversationId,
+            parentMessageId: response.id,
+            timestamp: (new Date()).getTime()
+        });
+    }
+
     return response;
+}
+
+async function formatConversationHistory(channel) {
+    const res = await app.client.conversations.history({channel});
+    return 'Resume la siguiente conversación:\n' +
+        res.messages
+            .reverse()
+            .filter(m => m.type === 'message')
+            .map(m => `<@${m.user}>: ${m.text}`)
+            .join('\n');
 }
